90d3c4db19d446f751955b88560fb4f49b2fd10d
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-10-04 12:24:43 +0100

    add outline for blueprints to controllers

--- controllers/parties_controller.py
+++ controllers/parties_controller.py
@@ -1,3 +1,12 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 
 import repos.party_repository as party_repository
+
+
+# index
+# show
+# new
+# create
+# edit
+# update
+# delete
--- controllers/players_controller.py
+++ controllers/players_controller.py
@@ -1,3 +1,13 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 
 import repos.player_repository as player_repository
+
+players_blueprint = Blueprint("players", __name__)
+
+# index
+# show
+# new
+# create
+# edit
+# update
+# delete
