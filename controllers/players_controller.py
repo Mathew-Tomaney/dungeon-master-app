5888adefe636acf39864fe60ac760424c29e6ac4
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-10-04 13:54:10 +0100

    add remaining routes to player blueprint.

--- controllers/players_controller.py
+++ controllers/players_controller.py
@@ -9,7 +9,7 @@ players_blueprint = Blueprint("players", __name__)
 def players():
     players = player_repository.select_all()
     return render_template("players/index.html", players=players)
-    
+
 # show
 @players_blueprint.route("/players/<id>")
 def show_player(id):
@@ -19,7 +19,38 @@ def show_player(id):
     return render_template("/players/show.html", player=player, parties=parties, characters=characters)
 
 # new
+@players_blueprint.route("/players/new")
+def new_player():
+    return render_template("/players/new.html")
+
 # create
+@players_blueprint.route("/players", methods=["POST"])
+def create_player():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    email = request.form["email"]
+    new_player = Player(first_name, last_name, email)
+    players_blueprint.save(new_player)
+    return redirect(players/<new_player.id>)
+
 # edit
+@players_blueprint.route("/players/<id>/edit")
+def edit_player(id):
+    player = player_repository.select(id)
+    return render_template("/players/edit.html" player=player)
+
 # update
+@players_blueprint.route("/players/<id>", methods=["POST"])
+def update_player(id):
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    email = request.form["email"]
+    player = Player(first_name, last_name, email, id)
+    player_repository.update(player)
+    return redirect("/players/<id>")
+
 # delete
+@players_blueprint.route("/players/<id>/delete", methods=["POST"])
+def delete_player(id):
+    player_repository.delete_id(id)
+    return redirect("/players")
