2cf48e08a189e077140505318b161825ab6f916f
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-10-04 12:18:53 +0100

    experiment with DELETE SET NULL. Not possible

--- controllers/characters_controller.py
+++ controllers/characters_controller.py
@@ -3,3 +3,12 @@ from flask import Blueprint, Flask, redirect, render_template, request
 import repos.character_repository as character_repository
 import repos.party_repository as party_repository
 import repos.player_repository as player_repository
+
+
+# index
+# show
+# new
+# create
+# edit
+# update
+# delete
