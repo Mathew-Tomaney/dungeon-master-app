a3b8968e3f6ececfa9e5899995a69292bd44a7bc
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-10-04 12:56:23 +0100

    add index and show to players_controller

--- controllers/players_controller.py
+++ controllers/players_controller.py
@@ -10,6 +10,13 @@ def players():
     players = player_repository.select_all()
     return render_template("players/index.html", players=players)
 # show
+@players_blueprint.route("/players/<id>")
+def show_player(id):
+    player = player_repository.select(id)
+    parties = player_repository.parties(id)
+    characters = player_repository.characters(id)
+    return render_template("/players/show.html", player=player, parties=parties, characters=characters)
+    
 # new
 # create
 # edit
